028eaa6305d4642c04877144cf8ba04668ab4882
kentn97 <kent24fearless@gmail.com>
2020-07-25 04:14:33 +0900

    added header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
+  <div class="page-container">
+    <TheHeader />
     <Nuxt />
   </div>
 </template>
 
-<style></style>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  width: 114rem;
+  margin: 3rem auto;
+
+  background-color: #fff;
+  box-shadow: 0 2rem 4rem rgba($color-black, 0.08);
+}
+</style>
